bde5dd3b8579ebf698b0bde3a6db1b74e929876c
이재용 <korom777755@gmail.com>
2026-01-26 03:20:09 +0900

    [Feat] RefreshToken 재발급 API Swagger 설명 추가

--- src/main/java/backend/onmoim/domain/auth/controller/AuthController.java
+++ src/main/java/backend/onmoim/domain/auth/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-public class AuthController {
+public class AuthController implements AuthControllerDocs {
 
     private final JwtUtil jwtUtil;
     private final AuthService authService;
