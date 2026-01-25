0a6b976598ee6b9db73353a31334c50ed86dc70c
tuna1025 <jo215098@gmail.com>
2026-01-25 18:07:49 +0900

    feat: 회원 도메인 스켈레톤 엔티티 추가
    - 이메일 인증기능을 구현하기 위해 id와 email필드만 임시 구현
    - 예약어 문제때문에 user대신 member로 구현, 이후 수정 가능

---
+++ src/main/java/backend/onmoim/domain/user/entity/Member.java
@@ -0,0 +1,21 @@
+package backend.onmoim.domain.user.entity;
+
+import backend.onmoim.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+}
