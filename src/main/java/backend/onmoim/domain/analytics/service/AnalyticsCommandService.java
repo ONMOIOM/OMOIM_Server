642877e9d993ada899c97e8928e5ac74b59ba3e0
koreanpasta <hg2374@daum.net>
2026-01-30 13:58:52 +0900

    [Feat] 서비스 단 제작

---
+++ src/main/java/backend/onmoim/domain/analytics/service/AnalyticsCommandService.java
@@ -0,0 +1,36 @@
+package backend.onmoim.domain.analytics.service;
+
+import backend.onmoim.domain.analytics.code.AnalyticsErrorCode;
+import backend.onmoim.domain.analytics.entity.Analytics;
+import backend.onmoim.domain.analytics.repository.AnalyticsRespository;
+import backend.onmoim.global.common.exception.GeneralException;
+import backend.onmoim.global.common.session.RedisSessionTracker;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class AnalyticsCommandService {
+
+    private final RedisSessionTracker redisSessionTracker;
+    private final AnalyticsRespository analyticsRespoitory;
+
+    public String sessionEnter(Long userId,Long eventId){
+        String sessionId=redisSessionTracker.enter(userId,eventId);
+
+        return sessionId;
+    }
+
+
+    public void enterCount(Long eventId){
+        LocalDate today = LocalDate.now();
+        Analytics analytics = analyticsRespoitory.findByEventIdAndDate(eventId, today)
+                .orElseThrow(() -> new GeneralException(AnalyticsErrorCode.BAD_EVENT_ID));
+
+        analytics.incrementClickCount();
+    }
+}
